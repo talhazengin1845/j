6f6c698f8ce4df69efa0d83435f8a4df4d96dc56
talhazengin1845 <talhazengin1845@gmail.com>
2024-01-17 06:50:48 +0300

    lml.

---
+++ Contacts/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Contacts;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
